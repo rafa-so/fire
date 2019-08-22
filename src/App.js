fa904ccb729e131d60457f99f5a389e6dadf0f1c
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-08-22 10:48:34 -0300

    Init project react with firebase
     - create this project
     - connect of firebase
     - rescue firebase's data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: 'Carregando...',
+      nome: '',
+      idade: ''
+    };
+
+    let firebaseConfig = {
+      apiKey: "AIzaSyAxiD6vqmIM_QA6HXNU7tBOTrdZsWtdNms",
+      authDomain: "reactapp-945b5.firebaseapp.com",
+      databaseURL: "https://reactapp-945b5.firebaseio.com",
+      projectId: "reactapp-945b5",
+      storageBucket: "",
+      messagingSenderId: "615588689768",
+      appId: "1:615588689768:web:76c9956acfdf3604"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // observer firebase node
+    // firebase.database().ref('token')
+    //   .on('value', (snapshot) => {
+    //     let state = this.state;
+    //     state.token = snapshot.val();
+
+    //     this.setState(state);
+    //   });
+    
+    // not observer firebase node
+    // firebase.database().ref('token')
+    //   .once('value').then((snapshot) => {
+    //     let state = this.state;
+    //     state.token = snapshot.val();
+
+    //     this.setState(state);
+    //   });
+
+    firebase.database()
+      .ref('Usuarios').child(1)
+      .on('value', (snapshot) => {
+        let state = this.state;
+        state.nome = snapshot.val().nome;
+        state.idade = snapshot.val().idade;
+
+        this.setState(state);
+      });
+
+  }
+
+  render(){
+    const { token, nome, idade } = this.state;
+    return (
+      <div>
+        <h1>Token: { token }</h1>
+        <h2>Nome: { nome }</h2>
+        <h2>Idade: { idade }</h2>
+      </div>
+    );
+  }
 }
-
-export default App;
