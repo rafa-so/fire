b5f108efe0cae0f728dc2df7c89e8dc09ca64355
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-08-22 12:46:37 -0300

    Add some functions
     - add new register in firebase
     - search one register in database
     - use 'once' method and not 'on' to observer database firebase

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ export default class App extends Component {
     this.state = {
       token: 'Carregando...',
       nome: '',
-      idade: ''
+      idade: '',
+      tokenInput: "",
+      nomeInput: "",
+      idadeInput: ""
     };
 
     let firebaseConfig = {
@@ -25,13 +28,13 @@ export default class App extends Component {
     firebase.initializeApp(firebaseConfig);
 
     // observer firebase node
-    // firebase.database().ref('token')
-    //   .on('value', (snapshot) => {
-    //     let state = this.state;
-    //     state.token = snapshot.val();
+    firebase.database().ref('token')
+      .on('value', (snapshot) => {
+        let state = this.state;
+        state.token = snapshot.val();
 
-    //     this.setState(state);
-    //   });
+        this.setState(state);
+      });
     
     // not observer firebase node
     // firebase.database().ref('token')
@@ -52,12 +55,54 @@ export default class App extends Component {
         this.setState(state);
       });
 
+      this.cadastrar = this.cadastrar.bind(this);
+  }
+
+  cadastrar(e){
+    // firebase.database().ref('token')
+    //  .set(this.state.tokenInput);
+
+    // firebase.database().ref('Usuarios')
+    //   .child(1).child('idade')
+    //   .set(this.state.tokenInput);
+
+    // firebase.database().ref('Usuarios')
+    // .child(1).child('cargo').set(this.state.tokenInput);
+
+
+    // firebase.database().ref('Usuarios')
+    // .child(1).child('cargo').remove();
+
+    let state = this.state;
+
+    let usuarios = firebase.database().ref('Usuarios');
+    let chave = usuarios.push().key;
+    usuarios.child(chave).set({
+      nome: state.nomeInput,
+      idade: state.idadeInput
+    });
+    
+    e.preventDefault();
   }
 
   render(){
     const { token, nome, idade } = this.state;
     return (
       <div>
+        <form onSubmit={ this.cadastrar }>
+          Nome:<br/>
+          <input type="text" value={ this.state.nomeInput } 
+            onChange={ (e) => this.setState({nomeInput: e.target.value}) } />
+          <br/>
+          Idade:<br />
+          <input type="text" value={ this.state.idadeInput } 
+            onChange={ (e) => this.setState({idadeInput: e.target.value}) } />
+          <br />
+          <button type="submit">
+            Cadastrar
+          </button>
+        </form>
+
         <h1>Token: { token }</h1>
         <h2>Nome: { nome }</h2>
         <h2>Idade: { idade }</h2>
